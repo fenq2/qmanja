55b1b961157289e3c64cc80d19325db9d1ef2720
Sergey Sochienko <59665883+fenq2@users.noreply.github.com>
2020-07-06 19:07:33 +0300

    Add files via upload

--- static/js/main.js
+++ static/js/main.js
@@ -57,6 +57,30 @@ var swiper2 = new Swiper('.testimonials-slider__container', {
     }
   }
 });
+
+var swiper2 = new Swiper('.profile-slider__container', {
+  slidesPerView: 1,
+  spaceBetween: 45,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev'
+  },
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false
+  },
+  loop: true,
+  breakpoints: {
+    700: {
+      slidesPerView: 2,
+      spaceBetween: 20
+    },
+    960: {
+      slidesPerView: 3,
+      spaceBetween: 40
+    }
+  }
+});
 $(window).scroll(function () {
   var height = $(window).scrollTop();
   /*Если сделали скролл на 100px задаём новый класс для header*/
